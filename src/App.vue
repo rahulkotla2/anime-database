4eea131a094e063a1b6abe3b3cd059b03fb6d851
rahulkotla2 <rahulkotla2@gmail.com>
2023-03-23 10:58:21 +0530

    Creation and styling the Header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="head">
+    <header>
+      <h1>The <strong>Anime</strong>Database</h1>
+
+      <form class="search-box">
+        <input type="search" class="search-field" placeholder="Search for an anime..." required>
+      </form>
+    </header>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+}
+
+header h1 {
+  color: #888;
+  font-size: 42px;
+  font-weight: 400;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-transform: uppercase;
+  margin-bottom: 30px;
+}
+
+header h1 strong {
+  color: #313131;
+}
+
+header h1:hover {
+  color: #313131;
+}
+
+.search-box {
+  display: flex;
+  justify-content: center;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+
+.search-field {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+
+  background-color: #f3f3f3;
+  box-shadow: 0px 4px 8px rgba(0,0,0,0.15);
+
+  display: block;
+  width: 100%;
+  max-width: 600px;
+  padding: 15px;
+  border-radius: 8px;
+  color: #313131;
+  font-size: 20px;
+  transition: 0.4s;
+}
+
+.search-field::placeholder{
+  color: #aaa;
+}
+
+.search-field:focus,.search-field:valid{
+  color: #fff;
+  background-color:#313131;
+  box-shadow: 0 0 0 rgba(0,0,0,0.15);
 }
 </style>
